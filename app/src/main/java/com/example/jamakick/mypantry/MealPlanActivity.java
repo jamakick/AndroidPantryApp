1ef979cddedf4a786902a23f42c206cec4b5f0cf
jamakick <jamakick@iu.edu>
2019-02-15 10:12:39 -0500

    Created 3 main pages and added DrawerLayout

--- app/src/main/java/com/example/jamakick/mypantry/MealPlanActivity.java
+++ app/src/main/java/com/example/jamakick/mypantry/MealPlanActivity.java
@@ -1,31 +1,25 @@
 package com.example.jamakick.mypantry;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MealPlanActivity extends AppCompatActivity {
 
     private DrawerLayout dlayout;
 
-    private static final int flag1 = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_meal_plan);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -50,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
                         if (id == R.id.nav_pantry) {
 
-
                         }
 
                         else if (id == R.id.nav_grocery) {
@@ -61,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
 
                         }
 
-
                         dlayout.closeDrawers();
                         return true;
                     }
@@ -69,30 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//
-//        //declare bundle
-//        Bundle myData = getIntent().getExtras();
-//
-//        if (myData == null) {
-//            increment = 0;
-//        }
-//        else {
-//            increment = myData.getInt(KEY_INC);
-//        }
-//
-//    }
-//
-//    @Override
-//    protected void onNewIntent(Intent intent) {
-//        super.onNewIntent(intent);
-//        setIntent(intent);
-//    }
-
-
 
 
     @Override
